bcec689e8f5efe5003bfcf313ebfbbf8d8840e16
Andreas Kirsch <blackhc@gmail.com>
2011-05-28 15:36:16 +0000

    more work done
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/AndreasKirsch@526 a63bd714-7e14-4b5e-94e7-302c8c8ff188

---
+++ src/cuda/kernel.cu
@@ -0,0 +1,9 @@
+#include "cutil_math.h"
+
+#include "moleculeStorage.cum"
+
+#include "pairTraverser.cum"
+
+#include "cellProcessor.cum"
+
+#include "globalStats.cum"
---
+++ src/cuda/moleculeStorage.h
@@ -0,0 +1,16 @@
+/*
+ * moleculeStorage.h
+ *
+ *  Created on: May 28, 2011
+ *      Author: andreas
+ */
+
+#ifndef MOLECULESTORAGE_H_
+#define MOLECULESTORAGE_H_
+
+struct MoleculeStorage {
+	void uploadState();
+	void downloadResults();
+};
+
+#endif /* MOLECULESTORAGE_H_ */
