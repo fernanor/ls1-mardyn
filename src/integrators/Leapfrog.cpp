1f4f0f3704926b06f1b87d39fd026c1adba77ae1
Andreas Kirsch <blackhc@gmail.com>
2011-08-22 10:49:58 +0000

    Remove the totally and utterly useless state machine from the Leapfrog. Ridiculed be thee who camest up with this brainfart!
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/AndreasKirsch@643 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/integrators/Leapfrog.cpp
+++ src/integrators/Leapfrog.cpp
@@ -12,9 +12,6 @@ using namespace std;
 using Log::global_log;
 
 Leapfrog::Leapfrog(double timestepLength) {
-	// set starting state
-	this->_state = 3;
-
 	this->_timestepLength = timestepLength;
 }
 
@@ -22,88 +19,52 @@ Leapfrog::~Leapfrog() {
 }
 
 void Leapfrog::eventForcesCalculated(ParticleContainer* molCont, Domain* domain) {
-	if (this->_state == 2) {
-		transition2to3(molCont, domain);
+	Molecule* tM;
+	map<int, unsigned long> N;
+	map<int, unsigned long> rotDOF;
+	map<int, double> summv2;
+	map<int, double> sumIw2;
+	double dt_half = 0.5 * this->_timestepLength;
+	if (domain->severalThermostats()) {
+		for (tM = molCont->begin(); tM != molCont->end(); tM = molCont->next()) {
+			int cid = tM->componentid();
+			int thermostat = domain->getThermostat(cid);
+			tM->upd_postF(dt_half, summv2[thermostat], sumIw2[thermostat]);
+			N[thermostat]++;
+			rotDOF[thermostat] += domain->getComponentRotDOF(cid);
+		}
+	}
+	else {
+		unsigned long Ngt = 0;
+		unsigned long rotDOFgt = 0;
+		double summv2gt = 0.0;
+		double sumIw2gt = 0.0;
+		for (tM = molCont->begin(); tM != molCont->end(); tM = molCont->next()) {
+			int cid = tM->componentid();
+			tM->upd_postF(dt_half, summv2gt, sumIw2gt);
+			assert(summv2gt >= 0.0);
+			Ngt++;
+			rotDOFgt += domain->getComponentRotDOF(cid);
+		}
+		N[0] = Ngt;
+		rotDOF[0] = rotDOFgt;
+		summv2[0] = summv2gt;
+		sumIw2[0] = sumIw2gt;
+	}
+	for (map<int, double>::iterator thermit = summv2.begin(); thermit != summv2.end(); thermit++) {
+		assert(thermit->second > 0);
+		domain->setLocalSummv2(thermit->second, thermit->first);
+		domain->setLocalSumIw2(sumIw2[thermit->first], thermit->first);
+		domain->setLocalNrotDOF(thermit->first, N[thermit->first], rotDOF[thermit->first]);
 	}
 }
 
 void Leapfrog::eventNewTimestep(ParticleContainer* molCont, Domain* domain) {
-	if (this->_state == 3) {
-		transition3to1(molCont, domain);
-		transition1to2(molCont, domain);
-	}
-}
-
-void Leapfrog::transition1to2(ParticleContainer* molCont, Domain* domain) {
-	if (this->_state == 1) {
-		Molecule* tempMolecule;
-		double vcorr = 2. - 1. / domain->getGlobalBetaTrans();
-		double Dcorr = 2. - 1. / domain->getGlobalBetaRot();
-		for (tempMolecule = molCont->begin(); tempMolecule != molCont->end(); tempMolecule = molCont->next()) {
-			tempMolecule->upd_preF(_timestepLength, vcorr, Dcorr);
-		}
-
-		this->_state = 2;
-	}
-	else {
-		global_log->error() << "Leapfrog::transition1to2(...): Wrong state for state transition" << endl;
-	}
-}
-
-void Leapfrog::transition2to3(ParticleContainer* molCont, Domain* domain) {
-	if (this->_state == 2) {
-		Molecule* tM;
-		map<int, unsigned long> N;
-		map<int, unsigned long> rotDOF;
-		map<int, double> summv2;
-		map<int, double> sumIw2;
-		double dt_half = 0.5 * this->_timestepLength;
-		if (domain->severalThermostats()) {
-			for (tM = molCont->begin(); tM != molCont->end(); tM = molCont->next()) {
-				int cid = tM->componentid();
-				int thermostat = domain->getThermostat(cid);
-				tM->upd_postF(dt_half, summv2[thermostat], sumIw2[thermostat]);
-				N[thermostat]++;
-				rotDOF[thermostat] += domain->getComponentRotDOF(cid);
-			}
-		}
-		else {
-			unsigned long Ngt = 0;
-			unsigned long rotDOFgt = 0;
-			double summv2gt = 0.0;
-			double sumIw2gt = 0.0;
-			for (tM = molCont->begin(); tM != molCont->end(); tM = molCont->next()) {
-				int cid = tM->componentid();
-				tM->upd_postF(dt_half, summv2gt, sumIw2gt);
-				assert(summv2gt >= 0.0);
-				Ngt++;
-				rotDOFgt += domain->getComponentRotDOF(cid);
-			}
-			N[0] = Ngt;
-			rotDOF[0] = rotDOFgt;
-			summv2[0] = summv2gt;
-			sumIw2[0] = sumIw2gt;
-		}
-		for (map<int, double>::iterator thermit = summv2.begin(); thermit != summv2.end(); thermit++) {
-			assert(thermit->second > 0);
-			domain->setLocalSummv2(thermit->second, thermit->first);
-			domain->setLocalSumIw2(sumIw2[thermit->first], thermit->first);
-			domain->setLocalNrotDOF(thermit->first, N[thermit->first], rotDOF[thermit->first]);
-		}
-
-		this->_state = 3;
-	}
-	else {
-		global_log->error() << "Leapfrog::transition2to3(...): Wrong state for state transition" << endl;
-	}
-}
-
-void Leapfrog::transition3to1(ParticleContainer* molCont, Domain* domain) {
-	if (this->_state == 3) {
-		this->_state = 1;
-	}
-	else {
-		global_log->error() << "Leapfrog::transition3to1(...): Wrong state for state transition" << endl;
+	Molecule* tempMolecule;
+	double vcorr = 2. - 1. / domain->getGlobalBetaTrans();
+	double Dcorr = 2. - 1. / domain->getGlobalBetaRot();
+	for (tempMolecule = molCont->begin(); tempMolecule != molCont->end(); tempMolecule = molCont->next()) {
+		tempMolecule->upd_preF(_timestepLength, vcorr, Dcorr);
 	}
 }
 
--- src/integrators/Leapfrog.h
+++ src/integrators/Leapfrog.h
@@ -7,24 +7,12 @@
 //! @author Martin Buchholz, Martin Bernreuther, et al.
 //!
 //! For details about the algorithm see David Fincham's paper "Leapfrog rotational algorithms"
-//! This Leapfrog integrator is implemented as a deterministic finite automaton (DFA):
-//! - A state of the DFA corresponds to a coarse step in the integration loop.
-//!   The following states are used:
-//!   - state 3: "starting state". The simulation has either just started, or the last steps
-//!     of a time step have just been done. In state 3, the leapfrog integrator waits for the start
-//!     of the next time time step
-//!   - state 1: a new time step has just begun, the integrator is ready to start integrating
-//!     (transition to state 2)
-//!   - state 2: the first part is finished, now forces are needed to continue the integration
-//! - consequently, the transitions do the following things:
-//!   - 3 to 1: Nothing to do
-//!   - 1 to 2: calculate "preF"
-//!   - 2 to 3: calculate "postF"
-//! - For the transition to another state, a condition has to be fulfilled
-//!     There are two possibilities: The integrator has to wait for external information
-//!     e.g. the new forces. In this case, the integrator is informed, that new forces
-//!     have been calculated. The automaton can then do all necessary computations
-//!     that have to be done do get from the current state to the next state
+//! The integrator responds to the following events:
+//!		new timestep -> call preF
+//! 	forces calculated -> call postF
+//!
+//! If you look for the actual leapfrog integration code, go look at preF/postF in Molecule.
+//! You ask: Seperation of Concerns? Who cares, fuck it! :)
 class Leapfrog : public Integrator {
 public:
 	//! The constructor
@@ -59,25 +47,5 @@ public:
 			unsigned zoscillator,
 			ParticleContainer* molCont
 	);
-
-private:
-
-	//! state in which the integrator is
-	int _state;
-
-	//! @brief calculate new positions and the first velocity halfstep
-	//!
-	//! This method also checks whether the state is 1. If so, the calculations are done and
-	//! the state is set to 2, otherwise, an error is printed
-	void transition1to2(ParticleContainer* molCont, Domain* domain);
-
-	//! @brief calculates the second halfstep for the velocity and angular momentum (postF)
-	//!
-	//! This method also checks whether the state is 2. If so, the calculations are done and
-	//! the state is set to 3, otherwise, an error is printed
-	void transition2to3(ParticleContainer* molCont, Domain* domain);
-	//! @brief checks whether the state is 3. If so, the state is set to 3, otherwise, an error is printed.
-	void transition3to1(ParticleContainer* molCont, Domain* domain);
-
 };
 #endif /*LEAPFROG_H_*/
