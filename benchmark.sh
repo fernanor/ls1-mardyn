23d4c1c524a5ea189ff45b4732b2f2bc1f89d108
Andreas Kirsch <blackhc@gmail.com>
2011-09-02 09:32:06 +0000

    add results for the first optimization
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/AndreasKirsch@670 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+NUM_WARPS=
+NUM_LOCAL_STORAGE_WARPS=
+MAX_REGISTER_COUNT=
+
 LOGFILE="benchmark.$(date +%Y.%m.%d.%H.%M.%S).log"
 
 function log {
@@ -15,14 +19,14 @@ function fatal_error {
 	exit 1
 }
 
-# CUDA_CONFIG NUM_WARPS
+# CUDA_CONFIG
 function build {
-	echo "Building $1 with $2 warps:\n"
+	echo "Building $1 with $NUM_WARPS warps${NUM_LOCAL_STORAGE_WARPS:+, $NUM_LOCAL_STORAGE_WARPS local storage warps}${MAX_REGISTER_COUNT:+, $MAX_REGISTER_COUNT max registers count}:\n"
 	make -C src clean
-	make -C src TARGET=RELEASE CUDA_CONFIG=$1 NUM_WARPS=$2
+	make -C src TARGET=RELEASE CUDA_CONFIG=$1 NUM_WARPS=$NUM_WARPS NUM_LOCAL_STORAGE_WARPS=${NUM_LOCAL_STORAGE_WARPS:-$NUM_WARPS} MAX_REGISTER_COUNT=${MAX_REGISTER_COUNT:-63} 
 
 	if [ $? != "0" ]; then
-		fatal_error "make -C src TARGET=RELEASE CUDA_CONFIG=$1 NUM_WARPS=$2 failed"
+		fatal_error "TARGET=RELEASE CUDA_CONFIG=$1 NUM_WARPS=$NUM_WARPS NUM_LOCAL_STORAGE_WARPS=${NUM_LOCAL_STORAGE_WARPS:-$NUM_WARPS} MAX_REGISTER_COUNT=${MAX_REGISTER_COUNT:-63} failed"
 	fi
 }
 
@@ -55,16 +59,24 @@ log "Benchmarking mode: $1"
 
 case $1 in
 	"no_constant_memory" )
-		CFGs=$(echo benchmark/lj_{1,2,4,8,16,32}0000.cfg benchmark/lj3d1_{1,5}0000.cfg benchmark/lj3d1_lj2d1_{1,5}0000.cfg)
+		CFGs=$(echo benchmark/lj_{1,2,4,8,16,32}0000.cfg benchmark/lj3d1_{1,5,10}0000.cfg benchmark/lj3d1_lj2d1_{1,5,10}0000.cfg)
 		
-		benchmark CUDA_DOUBLE_SORTED_NO_CONSTANT_MEMORY 1 "$CFGs"
-		benchmark CUDA_DOUBLE_SORTED 1 "$CFGs"
+		NUM_WARPS=1
+		
+		benchmark CUDA_DOUBLE_SORTED_NO_CONSTANT_MEMORY "$CFGs"
+		benchmark CUDA_DOUBLE_SORTED "$CFGs"
+		;;
+	"warp_count_witch_cache" )
+		CFGs=$(echo benchmark/lj_{1,2,4,8,16,32}0000.cfg)
+		for NUM_WARPS in {1,2,4,8}; do
+			benchmark CUDA_DOUBLE_SORTED "$CFGs"
+		done
 		;;
 	* )
-		for numWarps in {2..6}; do
-			benchmark CUDA_DOUBLE_SORTED $numWarps "$ALL_CFGs"
-			benchmark CUDA_DOUBLE_UNSORTED $numWarps "$ALL_CFGs"
-			benchmark CUDA_FLOAT_UNSORTED $numWarps "$ALL_CFGs"
+		for NUM_WARPS in {2..6}; do
+			benchmark CUDA_DOUBLE_SORTED "$ALL_CFGs"
+			benchmark CUDA_DOUBLE_UNSORTED "$ALL_CFGs"
+			benchmark CUDA_FLOAT_UNSORTED "$ALL_CFGs"
 		done
 		
 		benchmark NO_CUDA 1
