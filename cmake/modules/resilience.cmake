edcc813668b1808bfbf0dd45a47c1e0727a93a78
Oliver Fernandes <oliver.fernandes@visus.uni-stuttgart.de>
2018-11-12 14:01:12 +0100

    fixed minor error in resilience cmake module

--- cmake/modules/resilience.cmake
+++ cmake/modules/resilience.cmake
@@ -1,9 +1,9 @@
 # mpi
 option(ENABLE_RESILIENCE "Enables Resilience Plugin" OFF)
 if(ENABLE_RESILIENCE)
-    set(LZ4_ENABLED ON CACHE INTERNAL "Enables the module LZ4" PARENT_SCOPE)
+    set(LZ4_ENABLED ON CACHE INTERNAL "Enables the module LZ4")
     message(STATUS "Resilience Enabled")
 else()
-    set(LZ4_ENABLED OFF CACHE INTERNAL "Enables the module LZ4" PARENT_SCOPE)
+    set(LZ4_ENABLED OFF CACHE INTERNAL "Enables the module LZ4")
     message(STATUS "Resilience Disabled")
 endif()
